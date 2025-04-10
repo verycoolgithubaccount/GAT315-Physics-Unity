b2d135aab34afa2365979c93b039bebdc7a97e94
verycoolgithubaccount <tposselt@student.neumont.edu>
2025-04-10 11:26:41 -0600

    Sprites

--- Assets/2D/Scripts/CharacterController2D.cs
+++ Assets/2D/Scripts/CharacterController2D.cs
@@ -1,3 +1,5 @@
+using System;
+using UnityEditor.Animations;
 using UnityEngine;
 
 [RequireComponent (typeof(Rigidbody2D))]
@@ -5,6 +7,8 @@ public class CharacterController2D : MonoBehaviour
 {
     [SerializeField] float speed = 1.0f;
     [SerializeField] float jumpHeight = 2.0f;
+    [SerializeField] Animator animator;
+    [SerializeField] SpriteRenderer spriteRenderer;
 
     Rigidbody2D rb;
     Vector2 force;
@@ -26,6 +30,10 @@ public class CharacterController2D : MonoBehaviour
         {
             rb.AddForce(Vector2.up * Mathf.Sqrt(-2 * Physics.gravity.y * jumpHeight), ForceMode2D.Impulse);
         }
+
+        if (animator != null) animator.SetFloat("Speed", Math.Abs(direction.x));
+        if (direction.x > 0.05f) spriteRenderer.flipX = false;
+        else if (direction.x < -0.05f) spriteRenderer.flipX = false;
     }
 
     private void FixedUpdate()
